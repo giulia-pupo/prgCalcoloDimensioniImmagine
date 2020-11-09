c86df343351e2f0d2d9c82944f9bde547a3a6e21
Giulia <giuliapupo0@gmail.com>
2020-11-09 11:10:18 +0100

    CalcoloDimensioniImmagine

--- prgCalcoloDimensioniImmagine/Program.cs
+++ prgCalcoloDimensioniImmagine/Program.cs
@@ -6,12 +6,49 @@ namespace prgCalcoloDimensioniImmagine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Inserisci il primo valore:");
-            int n1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Inserisci il secondo valore:");
-            int n2 = Convert.ToInt32(Console.ReadLine());
-            int calcolo = ((n1 * n2) * 3) / 1024;
-            Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            int n1, n2, calcolo;
+            Console.WriteLine("Inserisci il numero di bit:");
+            int risposta = Convert.ToInt32(Console.ReadLine());
+            if (risposta == 1)
+            {
+                Console.WriteLine("Inserisci il primo valore:");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Inserisci il secondo valore:");
+                n2 = Convert.ToInt32(Console.ReadLine());
+                calcolo = ((n1 * n2) / 8) / 1024;
+                Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            }
+            else if (risposta == 8)
+            {
+                Console.WriteLine("Inserisci il primo valore:");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Inserisci il secondo valore:");
+                n2 = Convert.ToInt32(Console.ReadLine());
+                calcolo = (n1 * n2) / 1024;
+                Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            }
+            else if (risposta == 16)
+            {
+                Console.WriteLine("Inserisci il primo valore:");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Inserisci il secondo valore:");
+                n2 = Convert.ToInt32(Console.ReadLine());
+                calcolo = (n1 * n2) * 2 / 1024;
+                Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            }
+            else if (risposta == 16)
+            {
+                Console.WriteLine("Inserisci il primo valore:");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Inserisci il secondo valore:");
+                n2 = Convert.ToInt32(Console.ReadLine());
+                calcolo = (n1 * n2)*3 / 1024;
+                Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            }
+            else
+            {
+                Console.WriteLine("Vale solo per 1, 8, 16, 24 bit");
+            }
         }
     }
 }
